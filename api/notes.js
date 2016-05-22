4ce31948a8f889e99ed2f9acba28ff67b833c114
Ana Lu <luczynaki@gmail.com>
2016-05-22 12:37:28 -0500

    write out the endpoint behaviors

--- api/notes.js
+++ api/notes.js
@@ -16,21 +16,101 @@ module.exports = {
 //////
 
 function getAllNotes(request, response) {
-  response.send('GET all notes');
+  connectToNotesCollection(function(error, db) {
+    assert.equal(error, null);
+
+    var notes = db.collection('notes');
+    notes.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      
+      db.close();
+      console.log('GET notes');
+      
+      response.send(docs);
+    });
+  });
 }
 
 function makeNewNote(request, response) {
-  response.send('POST new note');
+  var note = new Note(request.body);
+
+  connectToNotesCollection(function(error, db) {
+    assert.equal(error, null);
+
+    db.collection('notes').insertOne(note);
+    db.close();
+    console.log('POST notes');
+
+    response.status(200).end();
+  });
 }
 
 function getNote(request, response) {
-  response.send('GET a note');
+  var directRequest = Number(request.params.nummer);
+  // TODO you might want a fancier query
+  // var extraParameters = request.query;
+
+  connectToNotesCollection(function(error, db) {
+    assert.equal(error, null);
+
+    var notes = db.collection('notes');
+
+    notes.find({'_id': directRequest}).toArray(function(err, doc) {
+      assert.equal(err, null);
+
+      db.close();
+      console.log('GET note');
+
+      response.send(doc);
+    });
+  });
 }
 
 function updateNote(request, response) {
-  response.send('PUT a note');
+  var directRequest = Number(request.params.nummer);
+  var data = request.body;
+
+  connectToNotesCollection(function(error, db) {
+    assert.equal(error, null);
+
+    var notes = db.collection('notes');
+
+    var thisOne = { '_id': directRequest };
+    var howToUpdate = { $set: data };
+    notes.updateOne(thisOne, howToUpdate, function(err, result) {
+      assert.equal(err, null);
+
+      db.close();
+      console.log('PUT note');
+
+      response.status(200).end();
+    });
+  });
 }
 
 function deleteNote(request, response) {
-  response.send('DELETE a note');
+  var directRequest = Number(request.params.nummer);
+
+  connectToNotesCollection(function(error, db) {
+    assert.equal(error, null);
+
+    var notes = db.collection('notes');
+
+    var thisOne = { '_id': directRequest };
+    notes.deleteOne(thisOne, function(err, result) {
+      assert.equal(err, null);
+      assert.equal(result.result.n, 1);
+
+      db.close();
+      console.log('DEL note');
+
+      response.status(200).end();
+    });
+  });
+}
+
+//////
+
+function connectToNotesCollection(callback) {
+  MongoClient.connect(url, callback);
 }
